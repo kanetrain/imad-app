754d435ecf7135a7375839d48d1c17d60bb11cfd
kanetrain <30623985+kanetrain@users.noreply.github.com>
2017-08-03 17:41:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,16 +13,16 @@ app.get('/article-one', function (req, res) {
   res.send('Article one requested and will be served here');
 });
 
-app.get'/article-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send('Article one requested and will be served here');
-})
+});
 
-app. get'/article-three',function (req, res) '{'
-res send('article three requested and will be served here'):
+app.get('/article-three', function (req, res) {
+res.send('article three requested and will be served here');
+});
 // Use 8080 only for local development if you already have apache running on 80
 
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-.
