b88fe6bb8bf5fd8694acc23a24cfacb2a9a08d16
kanetrain <30623985+kanetrain@users.noreply.github.com>
2017-08-04 10:35:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) 
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
+    res.sendFile(path.join(__dirname, 'ui','article-one.html'))
+);
 
 app.get('/article-two', function (req, res) {
-  res.send('Article one requested and will be served here');
+  res.send('Article Two requested and will be served here');
 });
 
 app.get('/article-three', function (req, res) {
